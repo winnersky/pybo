ef173eab0ce3a4f49531ec464e9a91d573381ee1
김한준 <yourbest.kim@gmail.com>
2021-10-21 22:06:17 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,8 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.209.60.165']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
 
 # 로컬에서 장고 서버 진행시 --settings=config.settings.local
 # 서버에서 장고 서버 진행시 --settings=config.settings.prod
